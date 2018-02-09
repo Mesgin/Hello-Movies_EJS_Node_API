12f674a4c65767d236408a8ce7b7e6cf409cb212
Mo Mesgin <mo.mesgin@gmail.com>
2018-02-08 23:48:17 -0800

    styling

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const request = require('request')
 const app = express()
 const port = process.argv[2] || 8080
 
@@ -6,30 +7,21 @@ app.set('view engine', 'ejs')
 
 app.use(express.static('./public'))
 
-app.get('/',(req,res)=>{
-    res.render('index', { movies: getMovies() })
-    // res.send('hello')
+app.get('/', (req, res) => {
+    res.render('index')
 })
 
-
-function getMovies() {
-    return {
-        title: 'Blade Runner',
-        year: '1982',
-        rated: 'R',
-        released: '25 June 1982',
-        runtime: '1h 57min',
-        genre: 'Sci-Fi, Thriller',
-        director: 'Ridley Scott',
-        writer: 'Hampton Fancher, David Peoples',
-        actors: 'Harrison Ford, Rutger Hauer, Sean Young, Edward James Olmos',
-        plot: 'A blade runner must pursue and try to terminate four replicants who stole a ship in space and have returned to Earth to find their creator.',
-        language: 'English',
-        country: 'USA, Hong Kong'
+app.get('/search', (req, res) => {
+    let searchTerm = req.query.searchTerm.toLowerCase()
+    if (!searchTerm){
+        res.render('index')
     }
-}
-
-
+    request(`https://api.themoviedb.org/3/search/movie?api_key=d730ddf7f4c9229a1877979dcfa57302&query=${searchTerm}`, (err, response, data) => {
+        const dataObject = JSON.parse(data)
+        if(err) console.log(err)
+        res.render('movie', { dataObject })
+    })
+})
 
 app.listen(port, () => {
     console.log(`Listening on port: ${port}`)
